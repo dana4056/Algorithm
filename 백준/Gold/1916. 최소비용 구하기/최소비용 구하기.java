8bed42c8ea67a11af189b5626de256508cff64e2
dana <54545026+dana4056@users.noreply.github.com>
2023-03-04 15:14:55 +0900

    [Gold V] Title: 최소비용 구하기, Time: 496 ms, Memory: 49468 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,105 +1,102 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
-    static int start, end;
-    static int N, M;
-    static int[] dp;
-    static boolean[] visited;
-    static Map<Integer, ArrayList<Node>> map = new HashMap<Integer, ArrayList<Node>>();
-    static Queue<Integer> q = new LinkedList<>();
-    static class Node {
-        int start;
-        int end;
-        int cost;
+	static int N, M;
+	static final int INF = Integer.MAX_VALUE;
+	static int[] distance;
+	static boolean[] visited;
+	static Queue<Node> q;
+	static HashMap<Integer, ArrayList<Node>> map;
+	// map에 키 값으로 이미 출발 정점을 알고 있기 때문에
+	// Node가 갖고 있어야 할 정보는 이 간선이 나아가는 다음 정점의 번호, 그 간선의 가중치
+	static class Node{
+		int to, cost;
+		public Node(int to, int cost){
+			this.to = to;
+			this.cost = cost;
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 
-        public Node(int s, int e, int c){
-            start = s;
-            end = e;
-            cost = c;
-        }
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
 
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
+		map = new HashMap<Integer, ArrayList<Node>>();
+		for(int i = 0; i < M; i++){
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			if(map.containsKey(from)){
+				map.get(from).add(new Node(to, cost));
+			}else {
+				ArrayList<Node> nodes = new ArrayList();
+				nodes.add(new Node(to, cost));
+				map.put(from, nodes);
+			}
+		}
 
-        dp = new int[N+1];
-        visited = new boolean[N+1];
-/****************************************/
-        Arrays.fill(dp, Integer.MAX_VALUE);
+		st = new StringTokenizer(br.readLine());
+		int start = Integer.parseInt(st.nextToken());
+		int end = Integer.parseInt(st.nextToken());
+		// 입력 종료
 
-        // 입력
-        for(int i = 0 ; i < M ; i++){
-            String[] sec = br.readLine().split(" ");
-            int s = Integer.parseInt(sec[0]);
-            int e = Integer.parseInt(sec[1]);
-            int c = Integer.parseInt(sec[2]);
 
-            Node node = new Node(s, e, c);
+		// 출발점부터 index 지점까지의 최단 거리를 기억하는 배열
+		distance = new int[N + 1];
+		visited = new boolean[N + 1];
 
-            if(map.containsKey(s)){
-                map.get(s).add(node);
-            }else {
-                ArrayList<Node> nodes = new ArrayList<Node>();
-                nodes.add(node);
-                map.put(s, nodes);
-            }
-        }
+		// 출발점으로부터 최단 거리만을 기억할 것이므로 Infinite 값으로 초기화
+		Arrays.fill(distance, INF);
+		// 출발점에서 출발점으로 향하는 거리는 0이므로 0으로 값 할당
+		distance[start] = 0;
 
-        String[] se = br.readLine().split(" ");
-        start = Integer.parseInt(se[0]);
-        end = Integer.parseInt(se[1]);
+		q = new LinkedList<>();
+		q.add(new Node(start, 0)); // 시작점, 시작점에서 시작점으로 가는 비용은 0
 
-        dp[start] = 0;
+		while(!q.isEmpty()){
+			// 일단 큐에서 꺼내면 현재 지점으로 판단
+			Node now = q.poll();
 
-        // 큐에 다음 노드 넣기
-        q.add(start);
+			// 방문한 지점이라면 다시 방문하지 않음
+			if(visited[now.to]) continue;
 
-        while(!q.isEmpty()){
-            updateDP(q.poll());
-        }
-        System.out.println(dp[end]);
-    }
+			// 현재 지점 방문 처리
+			visited[now.to] = true;
+			// 현재 지점과 인접한 노드가 존재한다면
+			if(map.get(now.to) != null){
+				// now.to : 현재 노드의 정점(즉 다음 노드를 기준으로 한다면 출발점)
+				// next : 현재(now) 간선이 연결되어 있는 다음 정점
+				for(Node next: map.get(now.to)){
+					// 만약 현재 정점까지의 최단거리 + 현재 정점으로부터 이어져있는 간선의 목적지까지의 비용 < 기억되고 있는 목적지까지의 최단거리
+					if(distance[now.to] + next.cost < distance[next.to]){
+						// 라면 목적지까지의 최단거리 갱신
+						distance[next.to] = distance[now.to] + next.cost;
+					}
+				}
+			}
+			// 다음 최단거리 노드 찾기
+			nextNode();
+		}
+		System.out.println(distance[end]);
+	}
 
-    private static void updateDP(int now) {
-/******************************************/
-        if(visited[now]) return;
-        visited[now] = true;
+	private static void nextNode() {
+		int min = INF;
+		int next = -1;
 
-        ArrayList<Node> nodes = map.get(now);
-
-        if(nodes != null){
-            for(Node node: nodes){
-                if(node.end == start) continue;
-
-                if(dp[now] + node.cost < dp[node.end]){
-                    dp[node.end] = dp[now] + node.cost;
-                }
-            }
-        }
-        nextNode();
-    }
-
-    // 방문하지 않은 노드 중에서 가장 적은 비용의 노드 선택
-    private static void nextNode() {
-        int min = Integer.MAX_VALUE;
-        int next = -1;
-
-        for(int i = 1 ; i <= N ; i++){
-            if(!visited[i] && dp[i] < min){
-                min = dp[i];
-                next = i;
-            }
-        }
-        if(next > 0) q.add(next);
-    }
+		for(int i = 1; i <= N; i++){
+			if(!visited[i] && distance[i] < min){
+				next = i;
+				min = distance[i];
+			}
+		}
+		if(next > 0) q.add(new Node(next, min));
+	}
 }
