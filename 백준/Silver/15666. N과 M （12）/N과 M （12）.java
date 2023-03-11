96c987f0377467104c40891252c0692badf3fd70
dana <54545026+dana4056@users.noreply.github.com>
2023-03-12 02:18:50 +0900

    [Silver II] Title: N과 M (12), Time: 352 ms, Memory: 27308 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// 중복 조합
+public class Main {
+	static int N, M;
+	static int[] nums;
+	static int[] select;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+	static List<String> selectList = new ArrayList<>();
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] NM = br.readLine().split(" ");
+		String[] nums_temp = br.readLine().split(" ");
+
+		N = Integer.parseInt(NM[0]);
+		M = Integer.parseInt(NM[1]);
+		nums = new int[N];			// 고를 숫자 리스트
+		select = new int[M];		// 고른 숫자 리스트
+		visited = new boolean[N];
+		for(int i = 0 ; i < N ; i++){
+			nums[i] = Integer.parseInt(nums_temp[i]);
+		}
+		Arrays.sort(nums);		// 오름차순으로 정렬
+
+		comb(0, 0);
+
+		System.out.print(sb);
+	}
+
+	private static void comb(int cnt, int start) {
+
+		if(cnt == M){
+			String str = Arrays.toString(select);
+			if(!selectList.contains(str)){
+				selectList.add(str);
+				for(int j = 0 ; j < M ; j++){
+					sb.append(select[j]).append(" ");
+				}
+				sb.append("\n");
+			}
+			return;
+		}
+
+		for(int i = start ; i < N ; i++){
+			select[cnt] = nums[i];
+
+			comb(cnt+1, i);
+		}
+	}
+}
