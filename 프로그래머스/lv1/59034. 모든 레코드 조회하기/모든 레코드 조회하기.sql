2db262aa518b5c1b72a44726054f8a34e2ead37f
dana <54545026+dana4056@users.noreply.github.com>
2023-04-05 19:54:08 +0900

    [level 1] Title: 모든 레코드 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/59034. 모든 레코드 조회하기/모든 레코드 조회하기.sql
@@ -0,0 +1,18 @@
+-- 코드를 입력하세요
+SELECT * 
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
+
+
+
+-- 모든 정보 출력
+-- 동물 ID 순으로 오름차순?
+
+
+
+-- ANIMAL_ID	      동물 ID
+-- ANIMAL_TYPE	      종
+-- DATETIME	          보호 시작일
+-- INTAKE_CONDITION	  보호 시작시 상태
+-- NAME	              이름
+-- SEX_UPON_INTAKE    성별 및 중성화여부
