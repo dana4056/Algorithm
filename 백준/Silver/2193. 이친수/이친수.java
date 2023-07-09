7751185d5691e7bb97a128e19842a45d080b3505
dana <54545026+dana4056@users.noreply.github.com>
2023-07-09 17:25:39 +0900

    [Silver III] Title: 이친수, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,9 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
         if(N == 1 || N == 2){
             System.out.println(1);
             return;
