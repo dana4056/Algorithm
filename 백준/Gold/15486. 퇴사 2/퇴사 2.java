b5694a32c8906c70cb00f11ed3735dc9643bdd5b
dana <54545026+dana4056@users.noreply.github.com>
2023-07-17 22:28:21 +0900

    [Gold V] Title: 퇴사 2, Time: 840 ms, Memory: 305564 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,26 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] dp = new int[N+2];
-        int[] time = new int[N+1];
-        int[] pay = new int[N+1];
-
-        for(int i = 1 ; i <= N ; i++ ){
-            String[] TP = br.readLine().split(" ");
-            time[i] = Integer.parseInt(TP[0]);
-            pay[i] = Integer.parseInt(TP[1]);
-        }
-
+        
         for(int i = 1 ; i <= N ; i++){
+            String[] TP = br.readLine().split(" ");
+            int time = Integer.parseInt(TP[0]);
+            int pay = Integer.parseInt(TP[1]);
+
             dp[i] = Math.max(dp[i], dp[i-1]);
-            if(i+time[i] <= N+1){
-                dp[i+time[i]] = Math.max(dp[i+time[i]], dp[i] + pay[i]);
+            if(i+time <= N+1){
+                dp[i+time] = Math.max(dp[i+time], dp[i] + pay);
             }
         }
         System.out.println(Math.max(dp[N], dp[N+1]));
