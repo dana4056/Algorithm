1e9d953e77fe235fdf8d1439d42c002e6c51eb37
dana <54545026+dana4056@users.noreply.github.com>
2023-07-26 21:56:26 +0900

    [Silver I] Title: 트리 순회, Time: 128 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N;
+    static char[][] graph;
+    static StringBuilder middle = new StringBuilder();
+    static StringBuilder before = new StringBuilder();
+    static StringBuilder after = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        graph = new char[N][2];
+
+        for(int i = 0 ;i < N ; i++){
+            String PC1C2 = br.readLine();
+            char parent = PC1C2.charAt(0);
+            char left = PC1C2.charAt(2);
+            char right = PC1C2.charAt(4);
+
+            graph[parent-65][0] = left;
+            graph[parent-65][1] = right;
+        }
+
+        DFS('A', 1);
+
+        System.out.println(before);
+        System.out.println(middle);
+        System.out.print(after);
+    }
+
+    private static void DFS(char ch, int cnt) {
+        before.append(ch);
+
+        char left = graph[ch-65][0];
+        char right = graph[ch-65][1];
+
+        if(left != '.'){
+            DFS(left, cnt+1);
+        }
+        middle.append(ch);
+
+        if(right != '.'){
+            DFS(right, cnt+1);
+        }
+        after.append(ch);
+    }
+}
