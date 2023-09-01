568e869c558b0841489b810aaf95f6555da19839
dana <54545026+dana4056@users.noreply.github.com>
2023-09-01 21:57:03 +0900

    [Gold III] Title: 줄 세우기, Time: 596 ms, Memory: 54496 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String[] NM = br.readLine().split(" ");
+        int N = Integer.parseInt(NM[0]);
+        int M = Integer.parseInt(NM[1]);
+        int[] inDegree = new int[N+1];
+        ArrayList<Integer>[] graph = new ArrayList[N+1];
+
+        for(int i = 1 ; i <= N ; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i = 0 ; i < M ; i++){
+            String[] AB = br.readLine().split(" ");
+            int A = Integer.parseInt(AB[0]);
+            int B = Integer.parseInt(AB[1]);
+
+            graph[A].add(B);
+            inDegree[B] += 1;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 1 ; i <= N ; i++){
+            if(inDegree[i] == 0) q.add(i);
+        }
+
+        for(int i = 0 ; i < N ; i++){
+            int now = q.poll();
+            sb.append(now).append(" ");
+
+            for(int next : graph[now]){
+                inDegree[next] -= 1;
+                if(inDegree[next] == 0) q.add(next);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
